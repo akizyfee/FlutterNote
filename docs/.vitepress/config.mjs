e53414736712cf7e8878535463a84141c47d6932
Pause <akidyfee@gmail.com>
2024-01-19 17:37:51 +0800

    fix: base位置

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/base/',
+  base: '/FlutterNote/',
   title: "My Flutter",
   description: "A VitePress Site",
   themeConfig: {
